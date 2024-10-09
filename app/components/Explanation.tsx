cb6c918c01beade875c6a5e41d6adc109a71c7da
Ronney Vargas <ronneyvargas@gmail.com>
2024-10-09 11:31:05 -0500

    Text-gray-700 added to the text for explanation.

--- app/components/Explanation.tsx
+++ app/components/Explanation.tsx
@@ -2,7 +2,7 @@ export default function Explanation({ correctAnswer }: { correctAnswer: string }
     return (
         <div className="border border-red-300 bg-red-50 p-4 rounded-lg mt-4">
             <h3 className="text-red-600 font-bold mb-2">Explanation</h3>
-            <p>{correctAnswer}</p>
+            <p className="text-gray-700">{correctAnswer}</p>
         </div>
     );
 }
